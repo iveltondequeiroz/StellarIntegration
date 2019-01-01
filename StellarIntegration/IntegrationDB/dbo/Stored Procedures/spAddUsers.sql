4f9e6f31ca139bc0fd30a381fe60a22645a44c5d
@ivelton2018 <ivelton2018@gmail.com>
2018-12-31 20:15:07 -0400

    unit tests project added

---
+++ StellarIntegration/IntegrationDB/dbo/Stored Procedures/spAddUsers.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[spAddUsers]
+	@Account varchar(56),
+	@Balance int = 0
+AS
+	INSERT INTO dbo.ExchangeUsers values(@Account, @Balance)
+
+RETURN 0
---
+++ StellarIntegration/IntegrationTests/DBTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class DBTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
